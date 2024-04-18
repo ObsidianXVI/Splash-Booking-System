7eebfb3e74d7b18d0d230c156b6893c0c8e802b2
ObsidianXVI <siddharth.chitikela@gmail.com>
2024-04-18 19:11:09 +0800

    v0.2.1++ hotfix

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "5c2f3d2f487967cd1f4a1cf629ed713c",
-"index.html": "a6b65c155b528fb5a4274ad15f71ee72",
-"/": "a6b65c155b528fb5a4274ad15f71ee72",
-"main.dart.js": "1ff4aca23d8bd88397a897c2ef04f109",
+"index.html": "606a23687062c3bff29b2872db68a6e0",
+"/": "606a23687062c3bff29b2872db68a6e0",
+"main.dart.js": "f0a14f836114d0610bd4e32f710735d6",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
