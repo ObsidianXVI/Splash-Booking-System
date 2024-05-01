9c00c108ad9f6e572b88bd4abf6eaa2b0457c427
ObsidianXVI <siddharth.chitikela@gmail.com>
2024-05-01 12:41:31 +0800

    deploy v19

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "5c2f3d2f487967cd1f4a1cf629ed713c",
-"index.html": "8e37998077d71dd636f3dc4e3d4ba07d",
-"/": "8e37998077d71dd636f3dc4e3d4ba07d",
-"main.dart.js": "40d60453ebd346708cf57574eff40ec2",
+"index.html": "06aa7dd971a9d717bc11bbfb09df55e5",
+"/": "06aa7dd971a9d717bc11bbfb09df55e5",
+"main.dart.js": "9f164ad24263d5aae8299b6e451238f6",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "40f755b5eeaa5926a826e6ae9e7d2a45",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
